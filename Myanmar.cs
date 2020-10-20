0c8828ace09cfe122c327692e18844140021a992
vimouzinItext <68127958+vimouzinItext@users.noreply.github.com>
2020-10-20 11:17:46 -0400

    Add files via upload

---
+++ Myanmar.cs
@@ -0,0 +1,31 @@
+﻿using iText.IO.Font;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.License;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.iText
+{
+    class Myanmar
+    {
+        static String licensePath = "license.xml";
+        static String outputPath = "outputPdf.pdf";
+        static String pathToFontFile = "fontFile.ttf";
+
+        public static void Main(String[] args)
+        {
+            LicenseKey.LoadLicenseFile(licensePath);
+            PdfDocument pdfDocument = new PdfDocument(new PdfWriter(outputPath));
+            Document document = new Document(pdfDocument);
+            PdfFont font = PdfFontFactory.CreateFont(pathToFontFile, PdfEncodings.IDENTITY_H);
+            document.SetFont(font);
+            String text = " ြန်မာနိုင်ငံ 􀀀တရားဝင်အားဖြ င့် ပြ ည်ထော င်စု သမ္မတ မြ န်မာနိုင်ငံတော် 􀀀သည် အရှေ့ တော င်အာရှတွင် ဧရိယာ";
+            document.Add(new Paragraph(text));
+            document.Close();
+        }
+    }
+}
